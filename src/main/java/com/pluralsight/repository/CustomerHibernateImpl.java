845227b2635ad59bd12e3cbbd86ed90e54eeae11
Ritesh Dalvi <riteshdalvi270@gmail.com>
2017-03-05 00:15:01 -0600

    Using properties in xml

--- src/main/java/com/pluralsight/repository/CustomerHibernateImpl.java
+++ src/main/java/com/pluralsight/repository/CustomerHibernateImpl.java
@@ -1,6 +1,7 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.object.CustomerObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,12 +12,16 @@ public class CustomerHibernateImpl implements CustomerHibernate {
 
     final CustomerObject customer = new CustomerObject();
 
+    @Value("${dbUsername}")
+    String dbUsername;
+
     public void setCustomer(final String firstName) {
 
         customer.setFirstName(firstName);
     }
 
     public String findCustomer() {
+        System.out.println(dbUsername);
        return customer.getFirstName();
     }
 }
